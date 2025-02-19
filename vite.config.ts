be758f8c95258ba797a9274e84c055b6c849f28e
Max <maxli88488@gmail.com>
2025-02-19 12:08:44 -0800

    remove sonner

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['svelte-sonner']
-	},
-	ssr: {
-		noExternal: ['svelte-sonner']
-	}
+	plugins: [sveltekit()]
 });
